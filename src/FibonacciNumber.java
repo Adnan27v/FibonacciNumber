c5d1e151e47c2a50ef8e0c07debd33ae522dd965
Adnan27v <adnanvaktapuriya@gmail.com>
2022-10-13 15:58:38 -0400

    "first commit"

--- src/FibonacciNumber.java
+++ src/FibonacciNumber.java
@@ -1,16 +1,13 @@
-public class FibonacciNumber
-{
-public static void main(String ar[])
-{
-int a=0,b=1,c=0;
-int n=Integer.parseInt(ar[0]);
-System.out.print(a+ " "+b);
-for(int i=1;i<n-1;i++)
-{
-c=a+b;
-a=b;
-b=c;
-System.out.print(" "+c);
-}
-}
+public class FibonacciNumber {
+    public static void main(String ar[]) {
+        int a = 0, b = 1, c = 0;
+        int n = Integer.parseInt(ar[0]);
+        System.out.print(a + " " + b);
+        for (int i = 1; i < n - 1; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+            System.out.print(" " + c);
+        }
+    }
 }
