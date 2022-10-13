ffd316eaddcddda9f62305e9505014a67e7be1b8
Adnan27v <adnanvaktapuriya@gmail.com>
2022-10-13 15:50:13 -0400

    first commit

--- src/FibonacciNumber.java
+++ src/FibonacciNumber.java
@@ -1,28 +1,16 @@
-import java.util.Scanner;
-
-public class FibonacciNumber {
-
-	static int fib(int n) {
-
-		if (n <= 1)
-			return n;
-
-		return fib(n - 1)
-				+ fib(n - 2);
-	}
-
-	public static void main(String args[]) {
-		Scanner scnr = new Scanner(System.in);
-		System.out.println("Please enter an integer to print fibonacci series(>1):");
-		int N = scnr.nextInt();
-        
-		System.out.println("The Fibonacci series till " + N + "th number of the series:");
-
-		for (int i = 0; i < N; i++) {
-
-			System.out.print(fib(i) + " ");
-		}
-
-		scnr.close();
-	}
+public class FibonacciNumber
+{
+public static void main(String ar[])
+{
+int a=0,b=1,c=0;
+int n=Integer.parseInt(ar[0]);
+System.out.print(a+ " "+b);
+for(int i=1;i<n-1;i++)
+{
+c=a+b;
+a=b;
+b=c;
+System.out.print(" "+c);
+}
+}
 }
